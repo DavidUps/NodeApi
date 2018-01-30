225fe735e2da9e318d7aa6d950c076ad189c06fc
DavidUps <15121996david>
2018-01-30 12:09:08 +0100

    node api

--- server.js
+++ server.js
@@ -1,52 +1,71 @@
 //BASE SETUP
-//========================================================================================
-//Llamar a los paquetes que necesites
+//==============================================================================
 
-var express     = require('express');    // Llama a express
-var app         = express();              // Define tus aplicaciones usando express
+//call the packages we need
+
+var express     = require('express');     //call express
+var app         = express();              //define our app using express
 var bodyParser  = require('body-parser');
-
 var mongoose    = require('mongoose');
-mongoose.connect('mongodb://<davidups>:<1234>@ds119018.mlab.com:19018/node_api');
 
 var Bear        = require('./app/models/bear');
-//Configura tu app usando bodyParser()
-//Esto lo usaremos para coger los datos desde el POST
 
+mongoose.connect('mongodb://dbAdmin:Unisys01@ds119018.mlab.com:19018/node_api', {
+  useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open');
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+//configura app use bodyParser()
+//this will let us get the dat from POST
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080       //Ponemos el puerto
+var port = process.env.PORT || 8080;       //Set our port
 
-//Rutas para la aplicaciones
-//========================================================================================
+//ROUTER FOR OUR API
+//==============================================================================
 
-var router = express.Router();            //coge la instancia de la express ruta
+var router = express.Router();            //get an instance of the express Router
 
-//Puente que usamos para usar las peticiones
+//middelware to use for all requests
 router.use(function(req,res,next){
-  //hacer loggin
+  //do loggin
   console.log('Something is happening');
-  next();                                 //Nos aseguramos que valla a otras rutas y no se pare ahi
-})
-
-//Testeamos la ruta para estar seguros de que nos funciona
-router.get('/', function(req, res){
-  res.json({message: 'Wellcome to your api'});
+  next();                                 //make sure we go to the next routers and don´t stop here
 });
 
-//mas rutas de tu api aparecerán ahí
-//Tus rutas que acaben en /Bears
-router.route('/bears')
+//test route to make sure everything is working ==> accessed at GET http://localhost:8080/api
+router.get('/', function(req,res){
+  res.json({ message: 'Welcome to your api'});
+});
 
-    // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
+//more routers for our API will happend here
+
+//on routers that en in /bears
+//------------------------------------------------------------------------------
+router.route('/bears')
+.post(function(req, res) {
 
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
 
         // save the bear and check for errors
         bear.save(function(err) {
+          console.log(err);
             if (err)
                 res.send(err);
 
@@ -54,14 +73,12 @@ router.route('/bears')
         });
 
     });
-//registrar tus Rutas
-//========================================================================================
-//todas tus rutas deberán estar prefijadas con /api
+
+//REGISTER OUR RUTES------------------------------------------------------------
+//all of our routers will be prefixed with /api
 app.use('/api', router);
 
-//Empezamos el server
-//========================================================================================
+//START THE SERVER
+//==============================================================================
 app.listen(port);
-console.log('Magic thinks in the port: ' +  port);
-
-//
+console.log('Magic happend on port: ' + port);
